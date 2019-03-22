743e624a172b91ed2718bea0537ba68a05757570
Jonathan Eriksson <jonaerik@lycksele.se>
2019-03-22 10:06:18 +0100

    Add project files.

---
+++ TicTacToe/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.CurrentPlayerIcon = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.CurrentPlayerIcon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(9, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 149);
+            this.button1.TabIndex = 0;
+            this.button1.Tag = "0";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.ButtonClick);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(165, 11);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 149);
+            this.button2.TabIndex = 1;
+            this.button2.Tag = "1";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.ButtonClick);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Transparent;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(321, 11);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(150, 149);
+            this.button3.TabIndex = 2;
+            this.button3.Tag = "2";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.ButtonClick);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Transparent;
+            this.button4.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Location = new System.Drawing.Point(9, 166);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(150, 149);
+            this.button4.TabIndex = 3;
+            this.button4.Tag = "3";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.ButtonClick);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Transparent;
+            this.button5.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Location = new System.Drawing.Point(165, 166);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(150, 149);
+            this.button5.TabIndex = 4;
+            this.button5.Tag = "4";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.ButtonClick);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Transparent;
+            this.button6.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Location = new System.Drawing.Point(321, 166);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(150, 149);
+            this.button6.TabIndex = 5;
+            this.button6.Tag = "5";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.ButtonClick);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.Transparent;
+            this.button7.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button7.Location = new System.Drawing.Point(9, 322);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(150, 149);
+            this.button7.TabIndex = 6;
+            this.button7.Tag = "6";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.ButtonClick);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.Transparent;
+            this.button8.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button8.Location = new System.Drawing.Point(165, 322);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(150, 149);
+            this.button8.TabIndex = 7;
+            this.button8.Tag = "7";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.ButtonClick);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.Transparent;
+            this.button9.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button9.Location = new System.Drawing.Point(321, 322);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(150, 149);
+            this.button9.TabIndex = 8;
+            this.button9.Tag = "8";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.ButtonClick);
+            // 
+            // button10
+            // 
+            this.button10.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button10.Location = new System.Drawing.Point(657, 35);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(100, 100);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "?";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button11.Location = new System.Drawing.Point(491, 35);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(150, 100);
+            this.button11.TabIndex = 10;
+            this.button11.Text = "RESTART";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(521, 156);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(206, 36);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Current Player";
+            // 
+            // CurrentPlayerIcon
+            // 
+            this.CurrentPlayerIcon.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.CurrentPlayerIcon.Location = new System.Drawing.Point(548, 217);
+            this.CurrentPlayerIcon.Name = "CurrentPlayerIcon";
+            this.CurrentPlayerIcon.Size = new System.Drawing.Size(150, 150);
+            this.CurrentPlayerIcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.CurrentPlayerIcon.TabIndex = 13;
+            this.CurrentPlayerIcon.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(773, 482);
+            this.Controls.Add(this.CurrentPlayerIcon);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Tic Tac Toe";
+            ((System.ComponentModel.ISupportInitialize)(this.CurrentPlayerIcon)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox CurrentPlayerIcon;
+    }
+}
+
---
+++ TicTacToe/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        static List<String> gameBoard = new List<String>(); //Initialize list as 3x3 board in order to right and down.
+        static int currentPlayer = 0; // Initialize variable for deciding starting player
+        public Form1()
+        {
+            InitializeComponent();
+            // Randomly select integer 1 or 2. 
+            Random rand = new Random();
+            currentPlayer = rand.Next(0, 2);
+            ClearBoard();
+        }
+        private void ClearBoard()
+        {
+            // Clear whole list in case of restart
+            gameBoard.Clear();
+            // Fill all indexes with numbers from 1 to 9 in ascending order, to make every index unlike each other and match their button's tag.
+            for (int i = 0; i < 10; i++)
+            {
+                gameBoard.Add((i.ToString()));
+                // Null every form that has a tag to reset board buttons
+                foreach (Control Y in this.Controls)
+                {
+                    Console.WriteLine(Y.Tag);
+                    if (Y.Tag != null)
+                    {
+                        Y.BackgroundImage = null;
+                    }
+                }
+            }
+            // Change current player image to their corresponding player
+            if (currentPlayer % 2 != 0)
+            {
+                CurrentPlayerIcon.Image = Properties.Resources.Xred;
+            }
+            else
+            {
+                CurrentPlayerIcon.Image = Properties.Resources.Oblue;
+            }
+        }
+        private void MatchResultInput(string CurrentWinner)
+        {
+            // Display winner and restart game if either player clicks on "OK" in the end message.
+            if (MessageBox.Show(CurrentWinner + " WON!" + "\n" + "Start Over?", "Game Over!", MessageBoxButtons.OKCancel) == DialogResult.OK)
+            {
+                ClearBoard();
+            }
+        }
+        private void ButtonClick(object sender, EventArgs e)
+        {
+            // Use button as object in further functions
+            Button b = (sender as Button);
+            // Get the button's tag and store it in a variable
+            int buttonTag = int.Parse(b.Tag.ToString());
+            DecidePlayerTurn(buttonTag, b);
+        }
+        private void DecidePlayerTurn(int tag, Button b)
+        {
+            // If button's matching index in list has not been changed
+            if (gameBoard[tag] == tag.ToString()) {
+                // Switch player every turn and use random int to choose starting player in 1st game
+                // Change current player image to their corresponding player
+                if (currentPlayer % 2 == 0)
+                {
+                    CheckBoard("O", tag, b);
+                    CurrentPlayerIcon.Image = Properties.Resources.Xred;
+                }
+                else
+                {
+                    CheckBoard("X", tag, b);
+                    CurrentPlayerIcon.Image = Properties.Resources.Oblue;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Taken!");
+            }
+        }
+        private void CheckBoard(string v, int tag, Button b)
+        {
+            // Change button's index in board array to the current player
+            gameBoard[tag] = v;
+            // If player is O or X
+            if (v == "O")
+            {
+                b.BackgroundImageLayout = ImageLayout.Stretch;
+                b.BackgroundImage = Properties.Resources.Oblue;
+            }
+            else
+            {
+                b.BackgroundImageLayout = ImageLayout.Stretch;
+                b.BackgroundImage = Properties.Resources.Xred;
+            }
+            // Check various combinations horizontally and vertically to see if a player achived 3 in a row
+            for (int i = 0; i < 3; i++)
+            {
+                if (gameBoard[0 + (3 * i)] == gameBoard[1 + (3 * i)] && gameBoard[1 + (3 * i)] == gameBoard[2 + (3 * i)])
+                {
+                    MatchResultInput(gameBoard[0 + (3 * i)]);
+                }
+                if (gameBoard[0 + (1 * i)] == gameBoard[3 + (1 * i)] && gameBoard[3 + (1 * i)] == gameBoard[6 + (1 * i)])
+                {
+                    MatchResultInput(gameBoard[3 + (3 * i)]);
+                }
+            }
+            // Check both diagonal combinations to see if a player achived 3 in a row
+            if (gameBoard[0] == gameBoard[4] && gameBoard[4] == gameBoard[8])
+            {
+                MatchResultInput(gameBoard[0]);
+            }
+            if (gameBoard[2] == gameBoard[4] && gameBoard[4] == gameBoard[6])
+            {
+                MatchResultInput(gameBoard[2]);
+            }
+            // Up the variable by one to change player next turn
+            currentPlayer++;
+            // Check if all slots are taken
+            if (
+                    (gameBoard[0] == "X" || gameBoard[0] == "O")
+                && (gameBoard[1] == "X" || gameBoard[1] == "O")
+                && (gameBoard[2] == "X" || gameBoard[2] == "O")
+                && (gameBoard[3] == "X" || gameBoard[3] == "O")
+                && (gameBoard[4] == "X" || gameBoard[4] == "O")
+                && (gameBoard[5] == "X" || gameBoard[5] == "O")
+                && (gameBoard[6] == "X" || gameBoard[6] == "O")
+                && (gameBoard[7] == "X" || gameBoard[7] == "O")
+                && (gameBoard[8] == "X" || gameBoard[8] == "O")
+                )
+            {
+                // Restarts game if either player clicks on "OK" in end message
+                if (MessageBox.Show(" DRAW!" + "\n" + "Start Over?", "Game Over!", MessageBoxButtons.OKCancel) == DialogResult.OK)
+                {
+                    ClearBoard();
+                }
+            }
+        }
+        // Display manual for the player
+        private void button10_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Rules\n\n1. The board/playing area is a grid that is 3 squares wide and 3 squares tall. \n2. There are two players, 'X' or 'O', and you will need to assign yourself to one of them. \n3. The player who is first to get 3 of their own marks in a row in a diagional, horizontal or vertical order will win. \n4. If all the slots are taken on the board then the game will result in a draw. \n\nHow to win? \n\nYour goal is to get 3 marks in a row, when you first set out your first mark you will then need to look ahead and make decisions that will benefit you in the future. This would for example be that if you as a player places a mark across the board that might give the incentive of the other player to block your path. The trick is to have the player not knowing what your next motive will be after you have placed your first mark.");
+
+        }
+        // Restarts the game if the player manually clicks on the restart button
+        private void button11_Click(object sender, EventArgs e)
+        {
+            ClearBoard();
+        }
+    }
+}
